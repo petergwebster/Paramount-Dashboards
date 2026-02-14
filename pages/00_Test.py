97835bc57b7ce8a249b40e8d31f7766f89c49500
petergwebster <peter@consultexllc.com>
2026-02-14 15:29:10 -0500

    Delete pages/00_Test.py

--- pages/00_Test.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Test Page", layout="wide")
-st.title("Test Page")
-st.write("If you can see this page in the sidebar, multipage navigation is working.")
