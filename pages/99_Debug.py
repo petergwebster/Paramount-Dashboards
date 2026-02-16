9d6f72a6c6df6ad4db27187e74edc9daad5c321f
petergwebster <peter@consultexllc.com>
2026-02-16 13:21:21 -0500

    Update 99_Debug.py

--- pages/99_Debug.py
+++ pages/99_Debug.py
@@ -1,1 +1,14 @@
+import os
+import streamlit as st
 
+st.set_page_config(page_title="Debug", layout="wide")
+
+st.title("Debug")
+st.write("cwd")
+st.write(os.getcwd())
+
+st.write("root files")
+st.write(sorted(os.listdir(".")))
+
+st.write("pages files")
+st.write(sorted(os.listdir("pages")))
